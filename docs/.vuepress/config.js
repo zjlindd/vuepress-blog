d2b2f6dbdf0eda1061448b0d4ac41296ecccec30
zhangjianliu <zhangjianliu@tuhu.cn>
2021-06-03 19:49:45 +0800

    删除非必要文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,11 @@ module.exports = {
     themeConfig:{
         nav: [
             {text: "主页", link: "/"      },
-            { text: "node", link: "/node/" },
-            { text: "前端", link: "/webframe/"},
-            { text: "数据库", link: "/database/"   },
-            { text: "android", link: "/android/"   },
-            { text: "面试问题", link: "/interview/" }
+        ],
+        sidebar: [
+            '/',
+            '/page-a',
+            ['/page-b', 'Explicit link text']
         ]
     }
 }
