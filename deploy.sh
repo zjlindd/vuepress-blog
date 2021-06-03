f19c0d8efcc3e1d2e537c7bf78707ac4284a4d07
张建留 <zhangjianliu@tuhu.cn>
2021-06-03 20:32:07 +0800

    配置

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # 如果你想要部署到 https://zhangjianliu.github.io
 git push -f git@github.com:zhangjianliu/zhangjianliu.github.io.git master
 
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:zhangjianliu/<REPO>.git master:gh-pages
+# 如果发布到 https://zhangjianliu.github.io/<REPO>  REPO=github上的项目
+git push -f git@github.com:zhangjianliu/vuePress-blog.git master:gh-pages
 
 cd -
